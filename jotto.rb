41d843e9911ff46b794567878cc2bbe983197ee8
benbotvinick <benbotvinick@gmail.com>
2017-11-05 14:45:13 -0500

    Made some progress

--- jotto.rb
+++ jotto.rb
@@ -1,25 +1,58 @@
-def pickWord()
-  word = nil
-  File.foreach("words.txt").each_with_index do |line, index|
-    word = line if rand < 1.0/(index+1)
-  end
-  return word
+def getGuess(myWords) # Pick a word, any word
+  guess = words[rand(0..(guesses.length - 1)]
+  return guess
 end
 
-def generateGuesses()
-  File.open("words.txt").each do |line|
-    if (line.split("").to_a.uniq.length) == 5
-      File.open("guesses.txt", "w") do |file|
-        file.write("#{line}")
-      end
+def getWords()
+  arr = Array.new
+  File.open("words.txt").each_line do |line|
+    arr.push(line)
+  end
+end
+
+def takeGuess(playerWords, words)
+  print "Guess a five letter word "
+  guess = gets.chomp
+  if playerWords.include?(guess) && words.include?(guess)
+    return guess
+  elsif playerWords.include?(guess) == false && words.include?(guess)
+    print "You guessed that word already. Try again! "
+    return takeGuess(playerWords, words)
+  elsif playerWords.include?(guess) && words.include?(guess) == false
+    print "That is not a word in the English language. Try again! "
+    return takeGuess(playerWords, words)
+  end
+end
+
+def inCommon(secret, playerGuess)
+  sum = 0
+  playerGuess.split(" ").to_a.each do |i|
+    if secret.split(" ").to_a.include?(i)
+      sum += 1
+    end
+  end
+  return sum
+end
+
+def play()
+  words = getWords()
+  myWords = getWords()
+  playerWords = getWords()
+  secret = words[rand(0..(guesses.length - 1)]
+  data = Hash.new
+  playerGuess = nil
+  while playerGuess != secret && answer.between(0..5)
+    playerGuess = takeGuess(playerWords, words)
+    playerWords.delete(playerGuess)
+    print "Your guess and my word have #{inCommon(secret, playerGuess)}"
+    myGuess = getGuess()
+    print "I guess...#{myGuess}. Enter 'true' or a number between 0 and 5 as your response."
+    answer = gets.chomp
+    answer = answer.to_i
+    if answer.between?(0..5)
+      data[myGuess] = answer
+    else
+      print "Yes! I win!"
     end
   end
 end
-
-
-def play()
-  word = pickWord()
-  guesses = nil
-end
-
-generateGuesses()
