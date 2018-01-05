6b598ff00a6a540bcff4a7ff5c053da781acceb5
Ben Botvinick <botvinick@new-host-12.home>
2018-01-04 19:29:09 -0500

    Scripting

--- Python/jotto.py
+++ Python/jotto.py
@@ -1,4 +1,4 @@
-from random import *
+from randomn import *
 import tensorflow as tf
 import numpy as np
 import os
@@ -6,39 +6,54 @@ import os
 class Computer(object):
     def __init__(self):
         self.alphabet = {
-            'a': 0, 'b': 0, 'c': 0,
-            'd': 0, 'e': 0, 'f': 0,
-            'g': 0, 'h': 0, 'i': 0,
-            'j': 0, 'k': 0, 'l': 0,
-            'm': 0, 'n': 0, 'o': 0,
-            'p': 0, 'q': 0, 'r': 0,
-            's': 0, 't': 0, 'u': 0,
-            'v': 0, 'w': 0, 'x': 0,
+            'a': 0, 'b': 0,
+            'c': 0, 'd': 0,
+            'e': 0, 'f': 0,
+            'g': 0, 'h': 0,
+            'i': 0, 'j': 0,
+            'k': 0, 'l': 0,
+            'm': 0, 'n': 0,
+            'o': 0, 'p': 0,
+            'q': 0, 'r': 0,
+            's': 0, 't': 0,
+            'u': 0, 'v': 0,
+            'w': 0, 'x': 0,
             'y': 0, 'z': 0
         }
-        self.words = [line.rstrip('\n') for line in open('words.txt')]
-        self.choice = self.words[randint(0, len(self.words)-1)]
+        self.words = [line.rstrip('\n') for line in open('word_files/words.txt')]
+        self.choice = random.choice(self.words)
+
+    def mostLetters(self):
+
 
 class Human(object):
     def __init__(self):
-        self.alphabet1 = {
-            'a': 0, 'b': 0, 'c': 0,
-            'd': 0, 'e': 0, 'f': 0,
-            'g': 0, 'h': 0, 'i': 0,
-            'j': 0, 'k': 0, 'l': 0,
-            'm': 0, 'n': 0, 'o': 0,
-            'p': 0, 'q': 0, 'r': 0,
-            's': 0, 't': 0, 'u': 0,
-            'v': 0, 'w': 0, 'x': 0,
+        self.alphabet = {
+            'a': 0, 'b': 0,
+            'c': 0, 'd': 0,
+            'e': 0, 'f': 0,
+            'g': 0, 'h': 0,
+            'i': 0, 'j': 0,
+            'k': 0, 'l': 0,
+            'm': 0, 'n': 0,
+            'o': 0, 'p': 0,
+            'q': 0, 'r': 0,
+            's': 0, 't': 0,
+            'u': 0, 'v': 0,
+            'w': 0, 'x': 0,
             'y': 0, 'z': 0
         }
-        self.words = [line.rstrip('\n') for line in open('words.txt')]
+        self.words = [line.rstrip('\n') for line in open('word_files/words.txt')]
 
-class HumanGame(object):
+class Self_Play(object):
     def __init__(self):
         computer = Computer()
         human = Human()
-        print(computer.choice)
+
+class Human_Play(object):
+    def __init__(self):
+        player1 = Computer()
+        player2 = Human()
 
 if __name__ == "__main__":
     # Empty for now
--- README.md
+++ README.md
@@ -1,8 +1,9 @@
 # Jotto
 ### Goals
-1. Beat eachother's programs.
+1. Beat each other's programs.
 2. Beat Matt Zipin.
 3. Beat Nigel Hamilton's (the big bully) program.
+4. Win the tournament!
 ### Tips
 1. Use a [hash](https://ruby-doc.org/core-2.4.2/Hash.html) in Ruby or a [dictionary](https://www.tutorialspoint.com/python/python_dictionary.htm) in Python.
 2. Look up some cheatsheets on data manipulation (You are **not** permitted to edit my word list!
