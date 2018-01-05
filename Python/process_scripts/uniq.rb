6bd2f5a4da442f0b1472c850acd3d8ba70ac7125
Ben Botvinick <botvinick@new-host-2.home>
2018-01-05 16:01:55 -0500

    Generated word files

--- Python/process_scripts/uniq.rb
+++ Python/process_scripts/uniq.rb
@@ -4,7 +4,7 @@ words.each_with_index do |word, index|
   words[index] = word.chop
 end
 
-File.open("../word_files/words_with_repeats.txt", "w+") do |f|
+File.open("../word_files/words_without_repeats.txt", "w+") do |f|
   words.each do |element|
     if (element.downcase !~ /([a-z]).*\1/)
       f.puts(element)
